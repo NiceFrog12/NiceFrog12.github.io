fcb313988ba5749c645a1326bc9ed88e923f5145
NiceFrog12 <sasha18062009@gmail.com>
2025-02-17 16:13:45 +0100

    added project slider

--- script.js
+++ script.js
@@ -33,3 +33,48 @@ languageSelector.addEventListener("change", (event) => {
     loadTranslations(event.target.value);
 });
 
+
+$(document).ready(function(){
+  // Function to check if the user is on a phone
+  function isMobile() {
+      return window.innerWidth <= 480; // Adjust based on your needs
+  }
+
+  // Initialize slick slider
+  $('.projects__boxes').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    prevArrow: "<img src='images/interface/Arrow_left.svg' class='arrow arrow--left'>",
+    nextArrow: "<img src='images/interface/Arrow_right.svg' class='arrow arrow--right'>",
+    autoplay: isMobile(), // Enable autoplay only if mobile
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          autoplay: true  // Ensure autoplay is enabled for this breakpoint
+        }
+      }
+    ]
+  });
+
+  // Update autoplay when resizing
+  $(window).resize(function(){
+    $('.projects__boxes').slick('slickSetOption', 'autoplay', isMobile(), true);
+  });
+});
+
